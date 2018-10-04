efc5c2fad990f65bf28482e9cfce51f065252132
Chendi.Xue <xuechendi@gmail.com>
2018-10-03 23:03:39 -0400

    [Feature]Add sitemap search
    Signed-off-by: Chendi.Xue <xuechendi@gmail.com>

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 gem "jekyll"
 group :jekyll_plugins do
 	gem 'github-pages'
-	gem 'jekyll-postfiles'
 	gem "jekyll-gist"
 	gem "jekyll-paginate"
 end
--- static/js/super-search.js
+++ static/js/super-search.js
@@ -1,5 +1,4 @@
 /*  Super Search
-    Author: Kushagra Gour (http://kushagragour.in)
     MIT Licensed
 */
 
@@ -11,129 +10,118 @@
         currentInputValue = '',
         lastSearchResultHash,
         posts = [];
+	
+	var store = [{
+    "title": "Documentation - Configuration & First Steps",
+    "link": "/docs/help/docmentation.html",
+    "image": null,
+    "date": "March 20, 2016",
+    "category": ["docs","help"],
+    "excerpt": "Configuration All configuration options are in the _config.yml file. Created by Patricia Mafra General Settings name: Your name. job_title: Your..."
+},{
+    "title": "Installation",
+    "link": "/docs/help/installation.html",
+    "image": null,
+    "date": "March 16, 2016",
+    "category": ["docs","help"],
+    "excerpt": "I assume you have already downloaded and installed Ruby. Here’s what you need to do next: Run gem install jekyll..."
+},{
+    "title": "What is Jekyll?",
+    "link": "/docs/what-is-jekyll.html",
+    "image": null,
+    "date": "March 15, 2016",
+    "category": ["docs"],
+    "excerpt": "Jekyll is a parsing engine bundled as a ruby gem used to build static websites from dynamic components such as..."
+},{
+    "title": "Sample Data",
+    "link": "/docs/sample-data.html",
+    "image": null,
+    "date": "March 11, 2016",
+    "category": ["docs"],
+    "excerpt": "Markdown (or Textile), Liquid, HTML &amp; CSS go in. Static sites come out ready for deployment. Headings Heading 1 Heading..."
+}]
 
-    // Changes XML to JSON
-    // Modified version from here: http://davidwalsh.name/convert-xml-json
-    function xmlToJson(xml) {
+	// Changes XML to JSON
+	function xmlToJson(xml) {		
+		// Create the return object
+		var obj = {};
 
-        // Create the return object
-        var obj = {};
+		if (xml.nodeType == 1) { // element
+			// do attributes
+			if (xml.attributes.length > 0) {
+			obj["@attributes"] = {};
+				for (var j = 0; j < xml.attributes.length; j++) {
+					var attribute = xml.attributes.item(j);
+					obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+				}
+			}
+		} else if (xml.nodeType == 3) { // text
+			obj = xml.nodeValue;
+		}
 
-        if (xml.nodeType == 1) { // element
-            // do attributes
-            if (xml.attributes.length > 0) {
-            obj["@attributes"] = {};
-                for (var j = 0; j < xml.attributes.length; j++) {
-                    var attribute = xml.attributes.item(j);
-                    obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-                }
-            }
-        } else if (xml.nodeType == 3) { // text
-            obj = xml.nodeValue;
-        }
+		// do children
+		if (xml.hasChildNodes()) {
+			for(var i = 0; i < xml.childNodes.length; i++) {
+				var item = xml.childNodes.item(i);
+				var nodeName = item.nodeName;
+				if (typeof(obj[nodeName]) == "undefined") {
+					obj[nodeName] = xmlToJson(item);
+				} else {
+					if (typeof(obj[nodeName].push) == "undefined") {
+						var old = obj[nodeName];
+						obj[nodeName] = [];
+						obj[nodeName].push(old);
+					}
+					obj[nodeName].push(xmlToJson(item));
+				}
+			}
+		}
+		return obj;
+	};
+	
+    $('#search-input').on('keypress', function (event) {
+		var keycode = (event.keyCode ? event.keyCode : event.which);
+		if( keycode != '13' ){
+			return;
+		}
+        var resultdiv = $('#js-search__results');
+        var query = $(this).val();
+		resultdiv.empty();
+        findInSitemap(query, resultdiv);
+	})
+	
+	$('#search-input').focusout(function() {
+		$('#js-search__results').hide();
+		$('.show-results-count').text(0 + ' Results');
+		$('#search-input').val("");
+		$('.show-results-count').removeClass("active");
+	})
 
-        // do children
-        // If all text nodes inside, get concatenated text from them.
-        var textNodes = [].slice.call(xml.childNodes).filter(function (node) { return node.nodeType === 3; });
-        if (xml.hasChildNodes() && xml.childNodes.length === textNodes.length) {
-            obj = [].slice.call(xml.childNodes).reduce(function (text, node) { return text + node.nodeValue; }, '');
-        }
-        else if (xml.hasChildNodes()) {
-            for(var i = 0; i < xml.childNodes.length; i++) {
-                var item = xml.childNodes.item(i);
-                var nodeName = item.nodeName;
-                if (typeof(obj[nodeName]) == "undefined") {
-                    obj[nodeName] = xmlToJson(item);
-                } else {
-                    if (typeof(obj[nodeName].push) == "undefined") {
-                        var old = obj[nodeName];
-                        obj[nodeName] = [];
-                        obj[nodeName].push(old);
-                    }
-                    obj[nodeName].push(xmlToJson(item));
-                }
-            }
-        }
-        return obj;
-    }
-
-    function getPostsFromXml(xml) {
-        var json = xmlToJson(xml);
-        return json.channel.item;
-    }
-
-    var xmlhttp=new XMLHttpRequest();
-    xmlhttp.open("GET","/sitemap.xml");
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState != 4) return;
-        if (xmlhttp.status != 200 && xmlhttp.status != 304) { return; }
-        var node = (new DOMParser).parseFromString(xmlhttp.responseText, 'text/xml');
-        node = node.children[0];
-        posts = getPostsFromXml(node);
-    }
-    xmlhttp.send();
-
-    window.toggleSearch = function toggleSearch() {
-        _gaq.push(['_trackEvent', 'supersearch', searchEl.classList.contains('is-active')]);
-        searchEl.classList.toggle('is-active');
-        if (searchEl.classList.contains('is-active')) {
-            // while opening
-            searchInputEl.value = '';
-        } else {
-            // while closing
-            searchResultsEl.classList.add('is-hidden');
-        }
-        setTimeout(function () {
-            searchInputEl.focus();
-        }, 210);
-    }
-
-    window.addEventListener('keyup', function onKeyPress(e) {
-        if (e.which === 27) {
-            toggleSearch();
-        }
-    });
-    window.addEventListener('keypress', function onKeyPress(e) {
-        if (e.which === 47 && !searchEl.classList.contains('is-active')) {
-            toggleSearch();
-        }
-    });
-
-    searchInputEl.addEventListener('input', function onInputChange() {
-        var currentResultHash, d;
-
-        currentInputValue = (searchInputEl.value + '').toLowerCase();
-        if (!currentInputValue || currentInputValue.length < 3) {
-            lastSearchResultHash = '';
-            searchResultsEl.classList.add('is-hidden');
-            return;
-        }
-        searchResultsEl.style.offsetWidth;
-
-        var matchingPosts;
-        // check the `posts` object is single or many objects.
-        // if posts.title === undefined, so posts is many objects.
-        if(posts.title === undefined) {
-          matchingPosts = posts.filter(function (post) {
-              if ((post.title + '').toLowerCase().indexOf(currentInputValue) !== -1 || (post.description + '').toLowerCase().indexOf(currentInputValue) !== -1) {
-                  return true;
-              }
-          });
-        }else {
-          matchingPosts = [posts]; // assign single object to Array
-        }
-        if (!matchingPosts.length) {
-            searchResultsEl.classList.add('is-hidden');
-        }
-        currentResultHash = matchingPosts.reduce(function(hash, post) { return post.title + hash; }, '');
-        if (matchingPosts.length && currentResultHash !== lastSearchResultHash) {
-            searchResultsEl.classList.remove('is-hidden');
-            searchResultsEl.innerHTML = matchingPosts.map(function (post) {
-                d = new Date(post.pubDate);
-                return '<li><a href="' + post.link + '">' + post.title + '<span class="search__result-date">' + d.toUTCString().replace(/.*(\d{2})\s+(\w{3})\s+(\d{4}).*/,'$2 $1, $3') + '</span></a></li>';
-            }).join('');
-        }
-        lastSearchResultHash = currentResultHash;
-    });
+	function findInSitemap (keyword, resultdiv) {
+		$.ajax({
+			url: "sitemap.xml",
+		}).done(function(data) {
+			var sitemap = xmlToJson(data);
+			console.log(sitemap);
+			
+			var result = [];
+			$.each(sitemap.rss.channel.item, function(key, val){
+				if (val.description["#text"].search(new RegExp(keyword,"i")) != -1 || val.title["#text"].search(new RegExp(keyword,"i")) != -1) {
+					result.push(val);
+				}
+			});
+			
+			$('.show-results-count').text(result.length + ' Results');
+			$('.show-results-count').addClass("active");
+			
+			for (var item in result) {
+				var searchitem = '<li><a href="'+ result[item].link["#text"]+'">'+result[item].title["#text"]+'</a></li>';
+				resultdiv.append(searchitem);
+			}
+			if (!resultdiv.is(':visible'))
+				resultdiv.show();
+			});		
+	}
+    
 
 })();
